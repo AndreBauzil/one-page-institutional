7c5355c0e43f01b995a429cd10ce1b83b1681859
Andre Bauzil <andregbauzil@gmail.com>
2024-01-24 16:38:28 -0300

    feat: add three functions, two for menu's behavior and one for header

---
+++ main.js
@@ -0,0 +1,26 @@
+// Toggle menu when click on icon, hamburguer and X
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', () => {
+        nav.classList.toggle('show')
+    })
+}
+
+// Hide menu when click on item
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', () => {
+        nav.classList.remove('show')
+    })
+}
+
+// Change page's header when use scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+    window.scrollY >= navHeight ? header.classList.add('scroll') : header.classList.remove('scroll')
+})
